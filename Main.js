fa73efe12e537e5fa203bd9530394f5c7bf0bb69
alruiz95 <alruiz300795@gmail.com>
2017-04-22 17:46:12 -0600

    InsertarArco

--- Main.js
+++ Main.js
@@ -8,10 +8,9 @@ class Nodo {
 }
 
 class Arco{
-  constructor(costo,origen,destino){
+  constructor(costo,destino){
     this.costo = costo;
-    this.origen = origen;
-    this.destino = destino
+    this.destino = destino;
   }
 }
 
@@ -24,15 +23,37 @@ function insertarNodo(nombre){
   nodos[pos] = nn;
 }
 
-function
+function buscar (nombre){
+  var i;
+  for (i = 0; i < nodos.length; i++){
+    if (nodos[i].nombre == nombre){
+      return nodos[i];
+    }
+  }
+  return null;
+}
 
-function 
+function insertarArco(peso, origen,destino){
+  var nodoOrigen = buscar(origen);
+  var nodoDestino = buscar(destino);
+  if ((nodoOrigen == null) || (nodoDestino == null)){
+    document.write("Error de Insercion de Arco:"+origen+" a "+destino);
+    return;
+  }
+  var na;
+  na = new Arco(peso,nodoDestino);
+  var pos = nodoOrigen.arcos.length;
+  nodoOrigen.arcos[pos] = na;
+}
 
 
 insertarNodo("A");
 insertarNodo("B");
 insertarNodo("C");
+insertarArco(10,"C","A");
 insertarNodo("D");
 
 var pos = nodos.length;
 document.write(pos);
+var n = buscar("C");
+document.write(n.arcos[0].costo);
