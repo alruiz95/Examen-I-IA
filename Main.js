e3e2986a0c1647ca20965726d6f8408448a2cff1
alruiz95 <alruiz300795@gmail.com>
2017-04-22 15:20:47 -0600

    Add Arco

--- Main.js
+++ Main.js
@@ -3,5 +3,14 @@
 class Nodo {
   constructor(nombre) {
     this.nombre = nombre;
+    this.arcos =  new Array();
+  }
+}
+
+class Arco{
+  constructor(costo,origen,destino){
+    this.costo = costo;
+    this.origen = origen;
+    this.destino = destino
   }
 }
