315c63eb71777b9a6a1051bd1a5b96d2c78cd544
alruiz95 <alruiz300795@gmail.com>
2017-04-22 21:01:30 -0600

    InsercionAutomatica Parcial y Validacion insertarArco

--- Main.js
+++ Main.js
@@ -18,6 +18,13 @@ class Arco{
     this.destino = destino;
   }
 }
+//Funciones Extras
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 //Funciones de Nodos
 
@@ -41,13 +48,35 @@ function buscar (nombre){
   return null;
 }
 
+function existeArco(origen,destino){
+  var nodoOrigen = buscar(origen);
+  if (nodoOrigen == null){
+    return false;
+  }
+
+  for (var i = 0; i<nodoOrigen.arcos.length; i++){
+    if (nodoOrigen.arcos[i].nombre == destino){
+      return true;
+    }
+  }
+
+  return false;
+
+}
+
 function insertarArco(peso, origen,destino){
   var nodoOrigen = buscar(origen);
   var nodoDestino = buscar(destino);
   if ((nodoOrigen == null) || (nodoDestino == null)){
-    document.write("Error de Insercion de Arco:"+origen+" a "+destino);
+    document.write("Error de Insercion de Arco:"+origen+" a "+destino=" -> No existe alguno de los nodos");
     return;
   }
+
+  if existeArco(){
+    document.write("Error de Insercion de Arco:"+origen+" a "+destino=" -> Ya existe este arco");
+    return;
+  }
+
   var na;
   na = new Arco(peso,nodoDestino);
   var pos = nodoOrigen.arcos.length;
@@ -56,29 +85,52 @@ function insertarArco(peso, origen,destino){
 
 function insercionAutomaticaNodos(cantidad){
   //creacion de nodos
-  var x = 0;
-  var y = 1;
-  var nombre = "";
-
+  
   var contadores = new Array();
+  contadores[0]=0;
 
   for (var i = 0; i < cantidad; i++) {
 
-      if (x>letrasInsertar.length){
-        x = 0;
-        y++;
+      var lastCont = contadores.length;
+      
+      for (var posConts = lastCont; posConts>=0 ; posConts--){
+
+        if (contadores[posConts] >= letrasInsertar.length){
+
+
+          contadores[posConts] = 0;
+
+
+          if (posConts == 0){
+            contadores[lastCont]=0;
+          }else{
+            contadores[posConts-1] ++;
+          }
+
+
+        }
+
       }
 
-      for (var )
+      var nombre = "";
+      for (var toIns = 0; toIns<contadores.length; toIns++){
+        nombre += letrasInsertar[contadores[toIns]];
+      }
 
-      nombre = letrasInsertar[0];+letrasInsertar[1]+letrasInsertar[2]+letrasInsertar[3]
+      insertarNodo(nombre);
 
-      var pos = nodos.length;
-      var nn;
-      nn = new Nodo(nombre);
-      nodos[pos] = nn;
+
+      contadores[lastCont-1] ++;
   }
 
+}
+
+function imprimirNodos(){
+  document.write("Imprimir: <br>");
+  var i;
+  for (i = 0; i < nodos.length; i++){
+    document.write(nodos[i].nombre+"<br>");
+  }
 }
 
 
@@ -89,15 +141,17 @@ function insercionAutomaticaNodos(cantidad){
 INSERTAR AQUI TODAS LAS FUNCIONES DE BUSQUEDA A IMPLEMENTAR
 */
 
+/*
 insertarNodo("A");
 insertarNodo("B");
 insertarNodo("C");
 insertarArco(10,"C","A");
 insertarNodo("D");
+*/
+
+insercionAutomaticaNodos(2000);
 
 
-var pos = letrasInsertar.length;
+var pos = nodos.length;
 document.write(pos);
-
-var n = buscar("C");
-document.write(n.arcos[0].costo);
+imprimirNodos();
