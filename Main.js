c0bac040e88fb23c1ec63b900685a0ba275152b9
Yeimag <marielaag.15@gmail.com>
2017-04-24 21:10:26 -0600

    bfs

--- Main.js
+++ Main.js
@@ -293,7 +293,7 @@ function astar(nodoInicial,nodoFinal){
       var pos = abiertos.length;
 
       var arcDestino = buscarH(listaActual);
-      var sumatoria = ((listaActual.distancia)+(arcDestino.distancia)+(arcDestino.euristica) -(arcDestino.origen.euristica));
+      var sumatoria = ((listaActual.costo)+(arcDestino.costo)+(arcDestino.euristica)-(arcDestino.origen.euristica));
       listaActual = listaActual + arcDestino.nombre;
       abiertos[pos] = (listaActual + sumatoria) ;
       abiertos[sumatoria].sort();
@@ -315,54 +315,29 @@ function buscarH (listaActual){
   }
 }
 
-/*
 
-
-/*function insertarNodoCerrados(lista){
-  var pos = cerrados.length;
-  cerrados[pos] = lista;
-}
-
-function insertarNodoAbiertos(nodoOrigen){
-  while(listaActual[listaActual.length-1]==nodoDestino.nombre){
+function BFS(nodoInicial, nodoFinal){
+  var cerrados = new Array();
+  var abiertos = new Array();
+  var listaActual = new Array();
+  while(listaActual[listaActual.length-1]!=nodoFinal.nombre){
     if (cerrados.length==null){
       var posi = cerrados.length;
-      cerrados[posi] = nodoOrigen;//nodo.origen no se a creado, es el que el usuario selecciona para iniciar la busqueda
+      cerrados[posi] = nodoInicial;
     }
     else{
-      var listaActual = Array;
       listaActual = cerrados[cerrados.length-1];
       var pos = abiertos.length;
 
       var arcDestino = buscarH(listaActual);
-      var sumatoria = listaActual.distancia+arcDestino.distancia;
-      listaActual = listaActual +arcDestino.nombre;
-      abiertos[pos] = (listaActual+sumatoria) ;
+      var sumatoria = ((listaActual.costo)+(arcDestino.costo);
+      listaActual = listaActual + arcDestino.nombre;
+      abiertos[pos] = (listaActual + sumatoria) ;
       abiertos[sumatoria].sort();
       cerrados[cerrados.length-1] = abiertos[0];
       abiertos[0].remove();
       document.write(abiertos); 
-
     }
   }
   
-}   
-
-function buscarH (listaActual){
-  var ultNodoLista;
-  ultNodoLista = listaActual[listaActual.length-1];
-  for (i = 0; i < arcos.length; i++){
-    if(arcos[i].origen==ultNodoLista){
-      return arcos[i].destino;
-    }
-  }
-}
-
-
-function insertarAbiertos(nombre){
-  if (buscar(nombre) != null){
-    document.write("No se puede insertar el nodo: '"+nombre+"'' por que ya existe");
-  }
- 
-}
-*/
+}  
